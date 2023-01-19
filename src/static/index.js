0645ebfa14fae722d2aa42114561e8176e42a96b
krossing95 <103511616+krossing95@users.noreply.github.com>
2023-01-19 04:50:26 -0800

    Update index.js

--- src/static/index.js
+++ src/static/index.js
@@ -1,3 +1,3 @@
 export const app_name = "AMANNEƐ";
-export const baseUrl = 'https://amannee.herokuapp.com/';
+export const baseUrl = 'https://amannee-server.onrender.com/';
 export const loader = "/images/loading.gif";
